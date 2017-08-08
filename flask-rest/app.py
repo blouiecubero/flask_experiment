35a36ed012771d1dc7268bc1bbd331c590ebca56
Louie Cubero <blouiecubero@gmail.com>
2017-08-08 23:17:54 +0800

    Added flask reqparse

--- flask-rest/app.py
+++ flask-rest/app.py
@@ -1,14 +1,26 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api,reqparse
+from flask_jwt import JWT, jwt_required
 
+from security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key = 'qweasdqwe'
 api = Api(app)
 
+jwt = JWT(app, authenticate, identity)
+
 items = []
 
 class Item(Resource):
+	parser = reqparse.RequestParser()
+	parser.add_argument('price',
+		type=float,
+		required=True,
+		help="required"
+	)
+
+	@jwt_required()
 	def get(self, name):
 		item = next(filter(lambda x: x['name'] == name, items), None)
 		return {'item': item}, 200 if item else 404
@@ -20,6 +32,24 @@ class Item(Resource):
 		item = {'name': name, 'price': data['price'] }
 		items.append(item)
 		return item
+
+	def delete(self, name):
+		global items
+		items = list(filter(lambda x: x['name'] != name, items))
+		return {'message': 'Item deleted'}
+
+	def put(self,name):
+		
+		data = Item.parser.parse_args()
+
+		item = next(filter(lambda x: x['name'] == name, items), None)
+		if item is None:
+			item = {'name': name, 'price': data['price']}
+			items.append(item)
+		else:
+			item.update(data)
+		return item
+
 
 class ItemList(Resource):
 	def get(self):
