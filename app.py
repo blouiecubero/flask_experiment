0f16a366f3fe3e071da8f0956ca37df9b3ffcf0d
Brian Cubero <blouiecubero@gmail.com>
2017-08-08 18:06:02 +0800

    Added routes

--- app.py
+++ app.py
@@ -2,8 +2,37 @@ from flask import Flask
 
 app = Flask(__name__)
 
+stores = [
+	{
+		'name': 'Store 1',
+		'items': [
+			{
+				'name': 'Item 1',
+				'price': 40
+			}
+		]
+	}
+]
+
 @app.route('/')
 def home():
 	return "Hello World!"
+
+@app.route('/store', method=['POST'])
+def create_store():
+	pass
+
+@app.route('/store/<string:name>')
+def get_store(name):
+	pass
+
+@app.route('/store/<string:name>/item', method=['POST'])
+def create_item_in_store(name):
+	pass
+
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+	pass
+
 
 app.run(port=5000)
